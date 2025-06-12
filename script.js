517cb0b98b21f8c312886e5026c8899623a63cdc
itstechsavvy <globaltechsavvy99@gmail.com>
2025-06-12 18:06:22 +0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Find the card scene element in the HTML
-const scene = document.querySelector('.scene');
-
-// Add an event listener that waits for a click on the scene
-scene.addEventListener('click', function() {
-  // When clicked, find the card element inside and toggle the 'is-flipped' class
-  scene.querySelector('.card').classList.toggle('is-flipped');
-});
